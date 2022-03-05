d01203817df9506c7c6fda96a17d349884307af5
Erick Chavez <Devofure@users.noreply.github.com>
2022-03-05 08:26:20 +0100

    added stage management

--- index.js
+++ index.js
@@ -11,26 +11,41 @@ try {
     const gradlePath = core.getInput('gradlePath');
     const versionCode = core.getInput('versionCode');
     const versionName = core.getInput('versionName');
+    const versionStage = core.getInput('versionStage');
+
+
     console.log(`Gradle Path : ${gradlePath}`);
     console.log(`Version Code : ${versionCode}`);
     console.log(`Version Name : ${versionName}`);
+    console.log(`Version stage : ${versionStage}`);
 
     fs.readFile(gradlePath, 'utf8', function (err, data) {
         newGradle = data;
-        if (versionCode.length > 0)
+        if (versionCode.length > 0) {
+            console.log(`Trying to override version code ${versionCode}`)
             newGradle = newGradle.replace(versionCodeRegexPattern, `$1${versionCode}`);
+        }
         else {
             const lastVersionCodeStr = newGradle.match(versionCodeRegexPattern)[2];
             const newVersionCode = parseInt(lastVersionCodeStr) + 1;
             newGradle = newGradle.replace(versionCodeRegexPattern, `$1${newVersionCode}`);
         }
-        if (versionName.length > 0)
-            newGradle = newGradle.replace(versionNameRegexPattern, `$1\"${versionName}\"`);
+
+        if (versionName.length > 0) {
+            if (versionStage.length > 0) {
+                currentVersionCode = newGradle.match(versionCodeRegexPattern)[2];
+                const newVersion = versionName + "-" + versionStage + "." + currentVersionCode
+                console.log(`Trying to override version name ${newVersion}`)
+                newGradle = newGradle.replace(versionNameRegexPattern, `$1\"${newVersion}\"`);
+            } else {
+                console.log(`Trying to override version name ${versionName}`)
+                newGradle = newGradle.replace(versionNameRegexPattern, `$1\"${versionName}\"`);
+            }
+        }
+
         fs.writeFile(gradlePath, newGradle, function (err) {
             if (err) throw err;
-            console.log(`Successfully override version code ${versionCode}`)
-            if (versionName.length > 0)
-                console.log(`Successfully override version name ${versionName}`)
+            console.log(`Successfully override the file`)
             core.setOutput("result", `Done`);
         });
     });
